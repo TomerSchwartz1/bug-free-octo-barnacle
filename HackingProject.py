6dd619450223832e1dfdd200589dd621ba030d00
TomerSchwartz1 <139005774+TomerSchwartz1@users.noreply.github.com>
2023-07-11 17:20:25 +0300

    Update HackingProject.py

--- HackingProject.py
+++ HackingProject.py
@@ -8,7 +8,7 @@ def wait_for_usb(original_path):
         if os.path.isdir(original_path):
             print("USB driver connected!")
             target_path1 = "C:/Users/maki/Desktop"
-            target_path2 = "C:/Users/maki/Desktop"
+            target_path2 = "C:/intel"
             if os.path.exists(original_path):
                 if os.path.exists(target_path1) and os.path.exists(target_path2):
                     shutil.copy2(f"{original_path}/MAP1.exe", f"{target_path1}/MAP1.exe")
